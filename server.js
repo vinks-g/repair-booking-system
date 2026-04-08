659fadda51ecf860b9f4c4d8a5c7d8ca5e155a22
Your Real Name <kevinngarimaina@gmail.com>
2026-04-08 12:02:19 +0300

    Final homepage polish

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ app.use(
       useDefaults: true,
       directives: {
         ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-        "script-src": ["'self'", "'unsafe-inline'"],
+        "script-src": [
+          "'self'",
+          "'unsafe-inline'",
+          "https://cdn.jsdelivr.net"
+        ],
         "script-src-attr": ["'unsafe-inline'"]
       },
     },
